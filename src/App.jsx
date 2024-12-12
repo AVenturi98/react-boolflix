f0d2db4100a0a5d691ad2dc78f8f08a4b6ab24a7
AVenturi98 <lssndrventuri@gmail.com>
2024-12-12 15:15:36 +0100

    ADD: method context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
 import GlobalContext from "./context/GlobalContext"
+import axios from "axios"
 import Index from './pages/Index.jsx'
 
 
 function App() {
 
+  const [films, setFilms] = useState([])
+
+  function fetchData() {
+    axios.get('https://api.themoviedb.org/3/search/movie', {
+      params: { query: 'batman', language: 'en-US', page: '1' },
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmE5NDkyYjA4MDczODYzN2Y1M2RmNmJmZmE2YjhjMyIsIm5iZiI6MTczNDAwMTk5My42MzkwMDAyLCJzdWIiOiI2NzVhYzU0OTlhZTUyNmQ1MDhhOWNmOGIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.5QxtZmHBD5OWY4MsxJKFi1Me51dzgXlbXp0-CsDINX8'
+      }
+    })
+      .then(res => {
+        console.log(res.data)
+        setFilms(res.data)
+      })
+      .catch(err => console.error(err))
+  }
+
   return (
     <>
-      {/* <GlobalContext.Provider > */}
-      <Index />
-      {/* </GlobalContext.Provider> */}
+      <GlobalContext.Provider value={{ films, fetchData }}>
+        <Index />
+      </GlobalContext.Provider>
     </>
   )
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,25 +1,28 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useContext, useEffect } from "react"
+import GlobalContext from "../context/GlobalContext"
 
 function Index() {
 
-    function fetchData() {
-        axios.get('https://api.themoviedb.org/3/search/movie', {
-            params: { query: 'batman', language: 'en-US', page: '1' },
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmE5NDkyYjA4MDczODYzN2Y1M2RmNmJmZmE2YjhjMyIsIm5iZiI6MTczNDAwMTk5My42MzkwMDAyLCJzdWIiOiI2NzVhYzU0OTlhZTUyNmQ1MDhhOWNmOGIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.5QxtZmHBD5OWY4MsxJKFi1Me51dzgXlbXp0-CsDINX8'
-            }
-        })
-            .then(res => {
-                console.log(res.data)
-            })
-            .catch(err => console.error(err))
-    }
+    const { films, fetchData } = useContext(GlobalContext)
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     return (
         <>
-
+            <div>
+                {
+                    films.map(film =>
+                        <li key={film.id}>
+                            <div>{film.title}</div>
+                            <div>{film.original_title}</div>
+                            <div>{film.original_language}</div>
+                            <div>{film.vote_average}</div>
+                        </li>
+                    )
+                }
+            </div>
         </>
     )
 }
