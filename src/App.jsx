e144a9cdf746aa66549a3e9affc4bf39c4b23ca7
AVenturi98 <lssndrventuri@gmail.com>
2024-12-12 19:12:20 +0100

    ADD: property search

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from "react"
-import GlobalContext from "./context/GlobalContext"
+import { useState, useEffect } from "react"
 import axios from "axios"
-import Index from './pages/Index.jsx'
 
 
 function App() {
 
   const [films, setFilms] = useState([])
-  const [titleFilm, setTitleFilm] = useState([])
-  // const [originalTitleFilm, setOriginalTitleFilm] = useState([])
-  // const [languageFilm, setLanguageFilm] = useState([])
-  // const [voteFilm, setVote] = useState([])
-
+  const [search, setSearch] = useState('')
 
   function fetchFilms() {
     axios.get('https://api.themoviedb.org/3/search/movie', {
-      params: { query: 'batman', language: 'en-US', page: '1' },
+      params: { query: search, language: 'it-IT', page: '1' },
       headers: {
         accept: 'application/json',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmE5NDkyYjA4MDczODYzN2Y1M2RmNmJmZmE2YjhjMyIsIm5iZiI6MTczNDAwMTk5My42MzkwMDAyLCJzdWIiOiI2NzVhYzU0OTlhZTUyNmQ1MDhhOWNmOGIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.5QxtZmHBD5OWY4MsxJKFi1Me51dzgXlbXp0-CsDINX8'
@@ -24,16 +18,30 @@ function App() {
       .then(res => {
         console.log(res.data.results)
         setFilms(res.data.results)
-        setTitleFilm(res.data.results.map(e => e.title))
       })
       .catch(err => console.error(err))
   }
 
+  useEffect(() => {
+    setFilms(films.filter((film) => film.title.includes(search)))
+
+  }, [search])
+
   return (
     <>
-      <GlobalContext.Provider value={{ films, fetchFilms, titleFilm }}>
-        <Index />
-      </GlobalContext.Provider>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" placeholder="cerca.." value={search} onChange={(e) => setSearch(e.target.value)} />
+        <input type="submit" onClick={() => fetchFilms()} value='Submit' />
+      </form>
+      <ul>{
+        films.map((el, i) =>
+          <li key={i}>
+            <div >{el.title}</div>
+            <div >{el.original_language}</div>
+            <div >{el.original_title}</div>
+            <div >{el.vote_average}</div>
+          </li>)
+      }</ul>
     </>
   )
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,17 +1,26 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import GlobalContext from "../context/GlobalContext"
 import Card from "../components/Card"
 
 function Index() {
 
-    const { films, fetchFilms, titleFIlm } = useContext(GlobalContext)
+    const { films, setFilms, fetchFilms } = useContext(GlobalContext)
+    const [search, setSearch] = useState('batman')
 
     useEffect(() => {
         fetchFilms()
     }, [])
 
+    useEffect(() => {
+        setFilms(films.filter((film) => film.title.includes(search)))
+    }, [search])
+
     return (
         <>
+            <form action="">
+                <input type="text" placeholder="cerca.." value={search} onChange={(e) => setSearch(e.target.value)} />
+                {/* <input type="submit" /> */}
+            </form>
             <ul>{
                 films.map((el, i) =>
                     <li key={i}>
